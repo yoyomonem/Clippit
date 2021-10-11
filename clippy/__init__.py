658759112918719b03920eed55a46aae4dac5f19
Youssef Nasr <yoyo.monem22@gmail.com>
2021-10-11 14:32:52 +0000

    Create Python package "clippy"

---
+++ clippy/__init__.py
@@ -0,0 +1,9 @@
+import time
+
+def speak_about(say):
+    print("When all else fails, bind some paper together. My name is Clippy.")
+
+speak_about(say=NotImplemented)
+
+AUTHOR_FIRST_NAME = "Youssef"
+AUTHOR_LAST_NAME = "Nasr"
