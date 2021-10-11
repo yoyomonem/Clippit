a729f40c6a4c73d76bc542847a102b57a2ece1ff
Youssef Nasr <yoyo.monem22@gmail.com>
2021-10-11 14:34:04 +0000

    Update __init__.py

--- clippy/__init__.py
+++ clippy/__init__.py
@@ -3,7 +3,7 @@ import time
 def speak_about(say):
     print("When all else fails, bind some paper together. My name is Clippy.")
 
-speak_about(say=NotImplemented)
+speak_about(say="Make Clippy say something!")
 
 AUTHOR_FIRST_NAME = "Youssef"
 AUTHOR_LAST_NAME = "Nasr"
