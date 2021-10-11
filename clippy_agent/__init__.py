6958348fecd4caa448d687351e6851f7fc108abf
Youssef Nasr <yoyo.monem22@gmail.com>
2021-10-11 14:42:00 +0000

    Make Clippy smart

--- clippy_agent/__init__.py
+++ clippy_agent/__init__.py
@@ -2,6 +2,10 @@ import time
 
 def speak_about(say):
     print("When all else fails, bind some paper together. My name is Clippy.")
+    time.sleep(2)
+    print("Why don't you try using Python?")
+    time.sleep(1)
+    print("It's that good.")
 
 AUTHOR_FIRST_NAME = "Youssef"
 AUTHOR_LAST_NAME = "Nasr"
